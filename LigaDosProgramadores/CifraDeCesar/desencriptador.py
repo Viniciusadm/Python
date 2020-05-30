fa0aa8c912b3a9bcb1d30b56830a9fa109d2a6ec
Vinícius <viniciusam.22@gmail.com>
2020-05-30 14:48:23 -0300

    Melhora do código

---
+++ LigaDosProgramadores/CifraDeCesar/desencriptador.py
@@ -0,0 +1,62 @@
+from random import randint
+
+class Desencriptar:
+	def __init__(self):
+		self.alfabeto = [x for x in 'k8ocdn0m16jxbre7t39pwhsvqzau4fyigl53']
+		self.letras = []
+		self.letrasEmbaralhadas = []
+		self.letrasJuntas = []
+
+	def receberFrase(self):
+		self.frase = str(input('Digite uma frase: ')).lower()
+	
+	def receberCifra(self):
+		try:
+			self.cifra = int(input('Digite o valor da cifra: '))
+		except ValueError:
+			print('Digite um número')
+			self.receberCifra()
+
+	def criarLista(self):
+		for self.letra in self.frase:
+			self.letras.append(self.letra)
+
+	def criptarFrase(self):
+		for self.letra in self.letras:
+			if self.letra not in ' ':
+				if self.letra in self.alfabeto:
+					if self.alfabeto.index(self.letra) + self.cifra >= 36:
+						if self.cifra >= 36:
+							self.index = (self.cifra + self.alfabeto.index(self.letra)) % 36
+							self.letrasEmbaralhadas.append(self.alfabeto[self.index])
+						else:
+							self.index = self.alfabeto.index(self.letra) + self.cifra - 36
+							self.letrasEmbaralhadas.append(self.alfabeto[self.index])
+					else:
+						self.letrasEmbaralhadas.append(self.alfabeto[self.alfabeto.index(self.letra) + self.cifra])
+				else:
+					self.letrasEmbaralhadas.append(self.letra)
+			else:
+				self.letrasEmbaralhadas.append('±')
+
+	def inserirIndice(self):
+		if self.cifra > 36:
+			self.cifra %= 36
+		self.posicaoAleatoria = randint(0, len(self.letrasEmbaralhadas) - 1)
+		self.letrasEmbaralhadas.insert(self.posicaoAleatoria, f'ß{self.cifra}')
+		self.letrasEmbaralhadas.insert(self.posicaoAleatoria + 2, '¶')
+		
+	def juntarLista(self):
+		self.letrasJuntas = ''.join(self.letrasEmbaralhadas)
+		print(self.letrasJuntas)
+
+	def main(self):
+		self.receberFrase()
+		self.receberCifra()
+		self.criarLista()
+		self.criptarFrase()
+		self.inserirIndice()
+		self.juntarLista()
+
+iniciar = Desencriptar()
+iniciar.main()
